79d9f9ad08ae91f3e5eae7f040a775aa2404be8f
Hoe Hooi Yan <hoehooiyan@gmail.com>
2020-06-28 18:47:36 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,21 @@
+echo "Hello ${whoami)! Let's get you set up!"
+
 sudo apt-get update
 
-echo 'installing curl and other essentials'
-sudo apt install curl build-essential libssl-dev
+echo 'installing curl'
+sudo apt-get install curl -y
+
+echo 'installing other essential'
+sudo apt-get install build-essential libssl-dev -y
+
+echo 'installing tool to handle clipboard via CLI'
+sudo apt-get install xclip -y
+
+echo 'installing vim'
+sudo apt-get install vim -y
 
 echo 'installing git'
-sudo apt install git -y
+sudo apt-get install git -y
 
 echo "What name do you want to use in GIT user.name?"
 read git_config_user_name
@@ -16,7 +27,18 @@ read git_config_user_email
 git config --global user.email $git_config_user_email
 clear
 
-echo "Generating a SSH key"
+echo "Can I set VIM as your default GIT editor for you? (y/n)"
+read git_core_editor_to_vim
+if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
+	git config --global core.editor vim
+else
+	echo "Okay, no problem. :) Let's move on!"
+fi
+
+echo 'installing hub'
+sudo apt-get install hub -y
+
+echo "generating SSH key"
 ssh-keygen -t rsa -b 4096 -C $git_config_user_email
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
@@ -26,11 +48,36 @@ sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s /bin/zsh
 
-echo 'installing tool to handle clipboard via CLI'
-sudo apt-get install xclip -y
+echo 'installing autosuggestions' 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+echo 'installing vscode'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install code -y # or code-insiders
+
+echo 'installing vscode extensions'
 
 
-curl https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash
+echo 'installing nvm'
+sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash)"
+
+export NVM_DIR="$HOME/.nvm" && (
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.zshrc
+nvm --version
 
 echo 'installing node'
 nvm install node
@@ -41,3 +88,8 @@ nvm install --lts
 echo 'set to use lts version'
 nvm use --lts
 
+node --version
+npm --version
+
+echo 'install npm global packages'
+npm install --global npm-check-updates npm-check-updates serve nodemon gatsby-cli gitmoji-cli
